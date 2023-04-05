0c7e8e9f13f54075eb61573dc055ee1a6520791b
杜一鸣 <601139086@qq.com>
2023-04-05 17:09:38 +0800

    first commit

--- testURL/ViewController.swift
+++ testURL/ViewController.swift
@@ -9,11 +9,32 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var urlLabel: UILabel!
+    // 模拟网络请求的过程
+    let array = (1...10).map{ ("\($0)号url",Int.random(in: 1...10)) }
+    let queue = OperationQueue()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func test(_ sender: Any) {
+        for i in 0..<10 {
+            let (name,duration) = self.array[i]
+            // 可以自己继承opeartion实现cancel网络请求的功能
+            let operation = BlockOperation { [weak self] in
+                print("执行\(name)，\(Thread.current.description)")
+                Thread.sleep(forTimeInterval: Double(duration))
+                DispatchQueue.main.async {
+                    if (self?.urlLabel.text?.count == 0) {
+                        self?.urlLabel.text = name
+                    }
+                }
+            }
+            self.queue.addOperation(operation)
+        }
+        
+    }
+    
 }
 
